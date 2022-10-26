0a8ebf8cbab34df6409170de7057852c62fad6eb
gvfc789@naver.com <gvfc789@naver.com>
2022-10-26 17:53:07 +0900

    sdsd

--- src/server.js
+++ src/server.js
@@ -1,35 +1,28 @@
 import express from "express";
 
-const PORT = 4000;
+const PORT = 5000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("<h1>dont give up!!</h1>");
+const gossipMiddleWare = (req, res, next) => {
+  console.log("Someone is going to : ${req.url}");
+  next();
 };
+
+const handleHome = (req, res) => {
+  return res.send("I love middleware");
+};
+app.use(gossipMiddleWare);
+//미들웨어 순서 유의 use - > get
+app.get("/", gossipMiddleWare, handleHome);
 
 const handleLogin = (req, res) => {
   return res.send("Have a nice lunch!!");
 };
 
-// 첫번쨰 칸은 선언, 두번째는 반응. 이 법칙을 기억하쇼!
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-
-// login에 입장하면 handlelogin을 작동
-//()이라는 함수를 넣어주어야 처리가 됨
-// ()=> 의 뜻. 이 함수시 console.log를 해주라.
-
 const handleListening = () =>
   console.log("Server listening on http://localhost:${PORT}");
 
+app.get("/login", handleLogin);
+
 app.listen(PORT, handleListening);
-
-// app.get 과 app.listen의 형식은 같다. port나 / , 그리고 그 뒤엔 함수
-// 뒤에 /lalalalala 붙이면 갈수 있지만 기본 홈은 절대 갈수가 업승ㅁ -> 브라우저가 get/request 보내고 있기에.
-
-// const handleClick = (event) => {
-
-// }
-
-// b.addEventListener("click", handleClick)
