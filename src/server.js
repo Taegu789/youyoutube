83b7e59acf3b0be6802b7696445e2c1a3cb29da6
gvfc789@naver.com <gvfc789@naver.com>
2022-10-28 14:09:07 +0900

    20221028

--- src/server.js
+++ src/server.js
@@ -4,18 +4,32 @@ const PORT = 5000;
 
 const app = express();
 
-const gossipMiddleWare = (req, res, next) => {
-  console.log("Someone is going to : ${req.url}");
+const logger = (req, res, next) => {
+  console.log("${req.method} ${req.url}");
   next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1> Not Allowed </h1>");
+  }
+  //protected 인걸 확인하면 not allowed 포르텍티드 아니면 next
+  console.log("Allowed, you may continue.");
+  next();
+};
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
 };
 
 const handleHome = (req, res) => {
   return res.send("I love middleware");
 };
-app.use(gossipMiddleWare);
-//미들웨어 순서 유의 use - > get
-app.get("/", gossipMiddleWare, handleHome);
-
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 const handleLogin = (req, res) => {
   return res.send("Have a nice lunch!!");
 };
